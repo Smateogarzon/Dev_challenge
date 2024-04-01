283ee0f51bbcda168fafea2a5287b7837c426306
Smateogarzon <mateogarzon1002@gmail.com>
2024-03-31 19:07:56 -0500

    feat: :sparkles: integracion del linter
    se agregaron las devpendencias del proyecto a si como la configiuracion de husky y el linter

--- challenge-dev/.eslintrc.cjs
+++ challenge-dev/.eslintrc.cjs
@@ -1,20 +1,24 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {browser: true, es2020: true},
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
   rules: {
+    'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      {allowConstantExport: true},
     ],
+    'prettier/prettier': ['error'],
+    'react/prop-types': 'off',
   },
-}
+};
