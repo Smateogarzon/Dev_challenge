dca0459eda837f4e40bbfd22aff0cb37aada2f15
Smateogarzon <mateogarzon1002@gmail.com>
2024-03-31 21:20:19 -0500

    feat: :sparkles: estilos body
    primer estilado del body de la app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react';
+import styles from './app.module.css';
+
+function App() {
+  // const [count, setCount] = useState < number > 0;
+
+  return (
+    <header>
+      <h1 className={styles.title}>
+        Rick
+        <span>and</span>
+        Morthy
+      </h1>
+    </header>
+  );
+}
+
+export default App;
