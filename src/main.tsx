b24e96a72ea8aca4f669de3c0d60bf31d5567634
Smateogarzon <mateogarzon1002@gmail.com>
2024-04-02 23:59:26 -0500

    fix: :art: deploy

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
       <ApolloProvider client={client}>
-        <BrowserRouter basename='/Dev_challenge'>
+        <BrowserRouter basename='/dev_challenge'>
           <App />
         </BrowserRouter>
       </ApolloProvider>
